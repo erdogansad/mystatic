dfa580abb5b3c85f879521c704f0f6c2dc2dc9ce
erdogansad <erdogan.sad@windowslive.com>
2023-10-17 01:28:22 +0300

    -

--- api/server.js
+++ api/server.js
@@ -14,7 +14,7 @@ server.use(cors());
 server.use(morgan("combined"));
 server.set("json spaces", 2);
 
-server.all("/", (req, res) => res.status(200).json("Persona's API System"));
+server.all("/", (req, res) => res.status(200).json("Sad's API System"));
 server.all("*", (req, res, next) => (req.method === "GET" ? next() : res.status(403).json({ error: "Forbidden" })));
 server.use(express.static(dir));
 server.all("*", (_, res) => res.status(403).json({ error: "not found" }));
